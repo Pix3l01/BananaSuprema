0ece274d7a51b59ea89acfeaa44d8c5850ce701e
Alessandro Pizzorni <pizzorni.alessandro@gmail.com>
2021-04-26 20:45:14 +0200

    Created and added class to load properties and added properties file to gitignore

---
+++ src/main/java/com/BiF/BananaSuprema/BananaSuprema/Bean/Configuration.java
@@ -0,0 +1,34 @@
+package com.BiF.BananaSuprema.BananaSuprema.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Configuration {
+
+    private static Configuration instance = null;
+    private Properties properties;
+
+    private Configuration() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream input = classLoader.getResourceAsStream("Properties.properties");
+        properties = new Properties();
+        try {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Configuration getInstance() {
+        if (instance == null) {
+            instance = new Configuration();
+        }
+        return instance;
+    }
+
+    public Properties getProperties(){
+        return properties;
+    }
+
+}
